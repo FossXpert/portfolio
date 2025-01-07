05bc908bcd82c7a6e39a4c701b6ab50a4f2c341f
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2025-01-07 19:39:28 +0000

    feat

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MO from './components/cp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
      <h1 className="text-5xl font-bold underline">
       Hello world!
+      <MO/>
     </h1>
     </>
   )
---
+++ my-project/src/components/cp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MO = (props: Props) => {
+  return (
+    <div>cp</div>
+  )
+}
+
+export default MO
