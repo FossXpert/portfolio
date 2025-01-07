7d78e5b6c0446565986090e0f61fcb084be47e77
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2025-01-07 19:14:58 +0000

    n

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+ 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-xl bg-gray-600'> nnnnnn </div>
+    </>
+  )
+}
+
+export default App
