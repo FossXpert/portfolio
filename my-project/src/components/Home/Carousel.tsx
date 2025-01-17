fdec6aeef89acb001ce7ddd3e790c452a1abc3c7
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2025-01-17 02:29:34 +0000

    bbbb

--- my-project/src/components/Home/Carousel.tsx
+++ my-project/src/components/Home/Carousel.tsx
@@ -5,8 +5,13 @@ const bo = 'border border-solid border-white'
 
 const Carousel = (props: Props) => {
   return (
-    <div className={`flex w-full h-[50%] mt-[5%] ${bo}`}>
-      hello
+    <div className={`flex w-full h-[60%] mt-[5%] ${bo}`}>
+      <div className={`${bo} flex flex-[1/4] w-[35%]`}>
+        <img src='../../assets/Vector.svg' width={100} height={100} />
+      </div>
+      <div className={`${bo} flex flex-[3/4] w-[65%]`}>
+
+      </div>
     </div>
   )
 }
