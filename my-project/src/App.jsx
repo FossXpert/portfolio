71362349b52d68d904dd103c10a19f09a8cbef48
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2025-01-27 14:25:23 +0000

    d

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
- import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-import "@fontsource/outfit"; // Default weight 400
-import "./index.css"; // Ensure Tailwind styles are applied
+import "./index.css"; 
 
 
 function App() {
