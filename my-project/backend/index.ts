021754b9eb24f0f6c04b680690748957d01a4b5c
rahulray8518 <rahulray8518@gmail.com>
2025-01-29 14:35:11 +0000

    b

--- my-project/backend/index.ts
+++ my-project/backend/index.ts
@@ -3,9 +3,17 @@ import express from 'express';
 import cors from 'cors';
 
 
+
+const PORT = 5000;
+
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:5173"], // Add your frontend URL here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,
+}));
+
 
 
 
---
+++ my-project/backend/routes/contactRouter.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const contactRouter = express.Router();
+
+
+
+contactRouter.post("/getcontactForm",)
