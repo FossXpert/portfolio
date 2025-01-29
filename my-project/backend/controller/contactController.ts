86e2f73e4dab3a92f980279ee32b836313cdb800
rahulray8518 <rahulray8518@gmail.com>
2025-01-29 18:48:56 +0000

    b

--- my-project/backend/controller/contactController.ts
+++ my-project/backend/controller/contactController.ts
@@ -1,7 +1,16 @@
+import { Request, Response } from "express"
+import mongoose from 'mongoose'
 
-
-export const contactController = async () => {
+export const contactController = async (req:Request,res:Response) => {
     try {
+        const {name,email,message} = req.body;
+        console.log("Received Contact Message:", { name, email, message });
+
+        if (!name || !email || !message) {
+          return res.status(400).json({ error: "All fields are required!" });
+        }
+
+        const data = await mongoose
         
     } catch (error) {
         
