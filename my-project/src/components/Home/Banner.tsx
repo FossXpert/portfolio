597bc6011406578ba799bdfbc4b2f0088bd7bbaf
FossXpert <FossXpert@outlook.com>
2025-01-31 07:21:26 +0530

    add download functionality for CV in Banner component and include jsPDF dependency

--- my-project/src/components/Home/Banner.tsx
+++ my-project/src/components/Home/Banner.tsx
@@ -7,6 +7,14 @@ type Props = {
   title:string;
   description:string;
 }
+const downloadPdf = () => {
+  const link = document.createElement("a");
+  link.href = "../../assets/Ravi_1.pdf"; // Path to the PDF in the public folder
+  link.download = "Resume.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
 const Banner:FC<Props> = ({title,description}) => {
   return (
@@ -16,7 +24,8 @@ const Banner:FC<Props> = ({title,description}) => {
                 <h1 className={` text-7xl p-2 `}>{title}</h1>
                 <p className={`mt-4 text-gray-400 p-2 font-light leading-[48px] text-[1.5rem]`}>{description}</p>
                 <div className={`flex gap-4 mt-5`}>
-                  <button className={`w-[184px] h-[46px] text-gray-900 bg-white rounded-full`}>Download CV</button>
+                  <button className={`w-[184px] h-[46px] text-gray-900 bg-white rounded-full`}
+                  onClick={()=>downloadPdf()}>Download CV</button>
                   <button className={`w-[197px] h-[46px] text-purple-300 ${bo} !border-[#A60683] rounded-full`}>See Experiences</button>
                 </div>
             </div>
