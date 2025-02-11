9d35cab9c5317b17ead260fdf2a0e96ac21b7c97
FossXpert <FossXpert@outlook.com>
2025-02-11 07:50:01 +0530

    fix: update PDF download link and replace old resume file

--- my-project/src/Utills/utill.ts
+++ my-project/src/Utills/utill.ts
@@ -1,6 +1,6 @@
 export const downloadPdf = () => {
     const link = document.createElement("a");
-    link.href = "/Ravi_1.pdf"; // Path to the PDF in the public folder
+    link.href = "/Resume-Rahul.pdf"; // Path to the PDF in the public folder
     link.download = "Resume.pdf";
     document.body.appendChild(link);
     link.click();
