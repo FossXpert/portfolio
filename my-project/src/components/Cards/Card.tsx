dec38d47ecb61aaf304a634ba918a80ea9957357
FossXpert <FossXpert@outlook.com>
2025-02-26 20:20:14 +0530

    fix: adjust Card component width for better responsiveness

--- my-project/src/components/Cards/Card.tsx
+++ my-project/src/components/Cards/Card.tsx
@@ -9,7 +9,7 @@ type Props = {
 const bo = 'border border-solid border-white'
 const Card = (props: Props) => {
   return (
-    <div className={` flex flex-col min-w-[380px] h-[237px] rounded-[60px] justify-center items-start pl-8 gap-2`} style={{ background: props.color }}>
+    <div className={` flex flex-col min-w-[220px] h-[237px] rounded-[60px] justify-center items-start pl-8 gap-2`} style={{ background: props.color }}>
       <img className={`bg-transparent`} src={props.icon} />
       <p className={`bg-transparent text-gray-800 font-bold text-[24px]`}>{props.name}</p>
     </div>
