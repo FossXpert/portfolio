5bbecea9963254f8f56b5421d65c7441c2debaa9
FossXpert <FossXpert@outlook.com>
2025-02-27 10:47:52 +0530

    feat: add additional allowed origins for CORS and configure Vercel deployment settings

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5173",
         "http://100.93.3.137:5173/",
-        "https://portofolio-rahul.vercel.app/"
+        "https://portofolio-rahul.vercel.app/",
+        "https://portfolio-rahul-rays-projects.vercel.app/",
+        "https://portfolio-gamma-vert-63.vercel.app/"
     ], // Add your frontend URL here
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
