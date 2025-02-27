74a708c81e4fe03e96ededa4909ef7dec52d3206
FossXpert <FossXpert@outlook.com>
2025-02-27 09:58:46 +0530

    fix: change default server port to 5000 and update allowed origins for CORS

--- backend/index.ts
+++ backend/index.ts
@@ -3,14 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './utill/db';
 import { contactController } from './controller/contactController';
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5173",
-        "https://portfolio-nine-hazel-11.vercel.app",
-        "http://100.93.3.137:5173/"
+        "http://100.93.3.137:5173/",
+        "https://portofolio-rahul.vercel.app/"
     ], // Add your frontend URL here
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
