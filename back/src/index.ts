c1bd4889351ee3d07295c93c99346baabcb46235
FossXpert <FossXpert@outlook.com>
2025-02-28 08:20:34 +0530

    feat: update CORS configuration to allow specific frontend origins

--- back/src/index.ts
+++ back/src/index.ts
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["*"
+    origin: ["http://localhost:5173",
+        "http://100.93.3.137:5173/",
+        "https://portofolio-rahul.vercel.app/",
+        "https://portfolio-rahul-rays-projects.vercel.app/",
+        "https://portfolio-gamma-vert-63.vercel.app/"
     ], // Add your frontend URL here
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
