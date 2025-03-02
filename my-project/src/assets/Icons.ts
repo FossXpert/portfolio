6d8db8ccbf749ac072432bba29446a656c4aa85b
FossXpert <FossXpert@outlook.com>
2025-03-02 19:20:26 +0530

    fix: add new social media icon and update header navigation, rename e-commerce project to e-learning platform with new image link

--- my-project/src/assets/Icons.ts
+++ my-project/src/assets/Icons.ts
@@ -2,12 +2,14 @@ import { FaGithubAlt, FaLinkedin } from "react-icons/fa";
 import { FaCodepen, FaFigma, FaMedium } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 import jsIcon from '../assets/icons-svg/js.svg'
+import { FaXTwitter } from "react-icons/fa6";
 
 const Icons: Record<string, IconType> = {
     github_icon: FaGithubAlt,
     linkedin_icon: FaLinkedin,
     medium_icon: FaMedium,
     figma_icon: FaFigma,
-    codepen_icon: FaCodepen
+    codepen_icon: FaCodepen,
+    x_icon:FaXTwitter
 };
 export default Icons;
